74cde699780a04e6c7dfa7378a01842cdcc3c3d7
jeri <jeri.chen0805@yahoo.com.tw>
2016-06-01 16:14:00 +0000

    mail set

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,3 +13,15 @@ Route::get('login', 'Auth\AuthController@getLogin');
 Route::post('login', 'Auth\AuthController@postLogin');
 Route::get('logout', 'Auth\AuthController@logout');
 
+// Password reset link request routes...
+Route::get('password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+
+// // Password reset routes...
+// Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+// Route::post('password/reset', 'Auth\PasswordController@postReset');
+
+// // Registration routes...
+// Route::get('auth/register', 'Auth\AuthController@getRegister');
+// Route::post('auth/register', 'Auth\AuthController@postRegister');
+
